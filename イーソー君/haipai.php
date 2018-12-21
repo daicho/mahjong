79b58f5b93d8f9de86a70d08d156827477996ca4
daicho <daichimax78@gmail.com>
2018-12-22 03:43:19 +0900

    2018-12-22対局

--- イーソー君/haipai.php
+++ イーソー君/haipai.php
@@ -4,12 +4,13 @@ define("HAI_WIDTH",  30);
 define("HAI_HEIGHT", 40);
 
 $haipai = $_GET["haipai"];
+$hai_cnt = floor(strlen($haipai) / 3);
 
 $img = new Imagick();
-$img->newImage(HAI_WIDTH * 14, HAI_HEIGHT, new ImagickPixel("white"));
+$img->newImage(HAI_WIDTH * $hai_cnt, HAI_HEIGHT, new ImagickPixel("white"));
 $img->setImageFormat("png");
 
-for ($i = 0; $i < 14; $i++) {
+for ($i = 0; $i < $hai_cnt; $i++) {
     $haifile = "mjhai/" . substr($haipai, $i * 3, 3) . ".png";
 	$hai = new Imagick($haifile);
 	$img->compositeImage($hai, Imagick::COMPOSITE_DEFAULT, HAI_WIDTH * $i, 0);
