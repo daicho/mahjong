96687fdd188f2cebab39083fcfe9aa7a8feef36a
daicho <daichimax78@gmail.com>
2019-05-24 13:12:15 +0900

    2019-05-24mjcmをC言語で実装

--- bat/mjcm.bat
+++
@@ -1,2 +0,0 @@
-git add -A
-git commit -m %date:/=-%%1
---
+++ bat/mjcm.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    time_t timer;
+    struct tm *local;
+    char commit[256];
+
+	// 9Žž‚Ü‚Å‚Íð“ú‚Ì“ú•t
+    timer = time(NULL) - 9 * 60 * 60;
+    local = localtime(&timer);
+
+    strftime(commit, 256, "git commit -m \"%F", local);
+    if (argc >= 2) strcat(commit, argv[1]);
+    strcat(commit, "\"");
+
+    system("git add -A");
+    system(commit);
+
+    return 0;
+}
